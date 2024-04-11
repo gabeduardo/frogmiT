9c6397723710533c5d6b934e4dbae87e1af2af2e
gabriel <gemcir13@gmail.com>
2024-04-10 23:58:22 -0400

    ruta y controller GET con filtrado por mag_type

---
+++ app/controllers/api/features_controller.rb
@@ -0,0 +1,42 @@
+class Api::FeaturesController < ApplicationController
+    def index
+        earthquakes = Feature.all
+    
+        # Filtro por mag_type (si se proporciona)
+        mag_types = params[:mag_type] ? params[:mag_type] : []
+        earthquakes = earthquakes.where(mag_type: mag_types) if mag_types.present?
+    
+    
+    
+        # Serializar los datos para que asi los pueda devolver en el formato especificado
+        render json: {
+          data: earthquakes.map { |earthquake| serialize_earthquake(earthquake) },
+         
+        }
+      end
+    
+      private
+  #   metodo para hacer la serializacion del objeto a rails 
+      def serialize_earthquake(earthquake)
+        {
+          id: earthquake.id,
+          type: 'feature',
+          attributes: {
+            external_id: earthquake.external_id,
+            magnitude: earthquake.magnitude,
+            place: earthquake.place,
+            time: earthquake.time,
+            tsunami: earthquake.tsunami,
+            mag_type: earthquake.mag_type,
+            title: earthquake.title,
+            coordinates: {
+              longitude: earthquake.longitude,
+              latitude: earthquake.latitude
+            }
+          },
+          links: {
+            external_url: earthquake.url
+          }
+        }
+      end
+end
