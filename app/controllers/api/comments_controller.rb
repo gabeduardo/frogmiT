622e5e6d1d1ae0c509d35d680fbecc9a4a0240a3
gabriel <gemcir13@gmail.com>
2024-04-11 19:25:36 -0400

    agregada condicion verficiación body de post no esté vacío

--- app/controllers/api/comments_controller.rb
+++ app/controllers/api/comments_controller.rb
@@ -3,13 +3,18 @@ class  Api::CommentsController < ApplicationController
   skip_before_action :verify_authenticity_token, only: [:create]
   def create
     @comment = @feature.comments.new(comment_params)
-    if @comment.save
-      render json: @comment, status: :created
-    else
-      render json: @comment.errors, status: :unprocessable_entity
-    end
+      if @comment.body.present?
+        if @comment.save
+          render json: @comment, status: :created
+        else
+          render json: @comment.errors, status: :unprocessable_entity
+        end
+      
+      else
+        render json: { error: "El campo body no puede estar vacío"},status: :unprocessable_entity
+      
+      end
   end
-
   private
 
   def set_feature
